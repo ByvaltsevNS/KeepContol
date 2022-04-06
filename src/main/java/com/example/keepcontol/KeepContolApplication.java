35d7b1c5e74de208c969fb3135ce323337b9e601
byval <byvaltsev1978@gmail.com>
2022-04-06 20:43:02 +0300

    Initial commit

---
+++ src/main/java/com/example/keepcontol/KeepContolApplication.java
@@ -0,0 +1,13 @@
+package com.example.keepcontol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepContolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeepContolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/keepcontol/KeepContolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keepcontol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepContolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
